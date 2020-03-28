73ae5b6ab8bdc97a1fc9a0c8598ea35fba878d44
Atavachron <vladimir.kalichuk@gmail.com>
2020-03-28 12:43:32 +0400

    Completed nav for mobile screens with CSS and JS

--- ResponsiveNavigation/main.js
+++ ResponsiveNavigation/main.js
@@ -1,1 +1,15 @@
 //
+const open = document.querySelector(".open");
+const exit = document.querySelector(".exit");
+const nav = document.querySelector("#nav");
+
+open.addEventListener("click", openNav);
+exit.addEventListener("click", closeNav);
+
+function closeNav() {
+  nav.style.visibility = "hidden";
+}
+
+function openNav() {
+  nav.style.visibility = "visible";
+}
