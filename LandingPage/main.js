a6584f4c39ab42066a9400e3b802078e14b569fb
Atavachron <vladimir.kalichuk@gmail.com>
2020-04-02 21:23:32 +0400

    Completed mobile layout

--- LandingPage/main.js
+++ LandingPage/main.js
@@ -1,1 +1,15 @@
 //
+
+const get = element => document.getElementById(element);
+
+const open = get("menu-btn");
+const close = get("exit-btn");
+const nav = get("nav");
+
+open.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
